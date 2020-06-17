3bd7258de3037d9c240193af9bf0728e9ece9142
Prince <chimeremezechukwudire@gmail.com>
2020-06-17 19:32:15 +0100

    interest script augmented

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,26 @@
-let principal = 0;
-let interestRate = 0;
-let timesCompounded = 0;
-let termOfLoan = 0;
-let amount = 0;
+    let principal = 0;
+    let interestRate = 0;
+    let timesCompounded = 0;
+    let term = 0;
+    let amount = 0;
 
-function simpleInterest() {
-    event.preventDefault();
-    var principal = parseFloat(document.getElementById("principalSimple").value);
-    var interestRate = parseFloat(document.getElementById("interestSimple").value);
-    interestRate = interestRate / 100;
-    var termOfLoan = parseFloat(document.getElementById("termSimple").value);
-    var simpleInt = principal * interestRate * termOfLoan;
-    var amount = (principal + simpleInt).toFixed(2);
-    document.getElementById("siOutput-01").innerHTML = "Interest: $" + simpleInt.toFixed(2);
-    document.getElementById("siOutput-02").innerHTML = "Total plus interest: $" + amount;
-}
+    const display = () => {
+        let txt = document.getElementById("amount").value;
+        let neudisplay = document.getElementById('neu-principal')
+        neudisplay.innerHTML = '₦' + ' ' + txt;
+    }
 
-const simpleInterest = () => {
-    event.preventDefault();
-}
+    const simpleInterest = () => {
+        event.preventDefault();
+
+        let principal = parseFloat(document.getElementById("amount").value);
+        let interestRate = parseFloat(document.getElementById("rate").value)
+        interestRate = interestRate / 100;
+        let term = parseFloat(document.getElementById("term").value);
+        let simpleInt = principal * interestRate * term;
+        let amount = (principal + simpleInt).toFixed(2);
+
+        document.getElementById("interestpayable").innerHTML = "₦" + " " + simpleInt.toFixed(2);
+        document.getElementById("amountpayable").innerHTML = "₦ " + " " + amount;
+
+    }
